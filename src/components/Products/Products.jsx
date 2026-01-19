e3c1bc00509127bebe00b73321282375d27e5d2c
Prithviisss <prithvis6002@gmail.com>
2026-01-19 21:20:19 +0530

    added clicking for the buttons in product section

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Heading from '../Heading/Heading'
 
 const Products = () => {
-    const categories=['ALl','Fruits','Vegetables','Dairy','SeaFood']
+    const categories=['All','Fruits','Vegetables','Dairy','SeaFood']
+    const [activeTab,setActiveTab]=useState('All');
   return (
     <section>
     <div className='max-w-[1400px] mx-auto px-10 py-20'>
         <Heading highlight="Our" heading="Products" />
-        <div>
+        <div className='flex gap-3 justify-center mt-10 '>
             {categories.map(category=>{
                 return(
-                    <button key={category}>
+                    <button key={category} className={`bg-zinc-100 px-5 py-2 text-lg rounded-lg
+                        ${activeTab===category?'bg-gradient-to-b from-orange-400 to-orange-500 text-white':'bg-zinc-100'}`}
+                        onClick={()=>setActiveTab(category)}>
                         {category}
                     </button>
                 )

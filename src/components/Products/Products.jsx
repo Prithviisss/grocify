ba828e54717a8d5d7433b1a9f7f461464c881747
Prithviisss <prithvis6002@gmail.com>
2026-01-20 23:00:03 +0530

    responsiveness of the website

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -18,7 +18,7 @@ const Products = () => {
     <section>
     <div className='max-w-[1400px] mx-auto px-10 py-20'>
         <Heading highlight="Our" heading="Products" />
-        <div className='flex gap-3 justify-center mt-10 '>
+        <div className='flex flex-wrap gap-3 justify-center mt-10 '>
             {categories.map(category=>{
                 return(
                     <button key={category} className={`bg-zinc-100 px-5 py-2 text-lg rounded-lg cursor-pointer
@@ -29,7 +29,7 @@ const Products = () => {
                 )
             })}
         </div>
-        <div className='grid grid-cols-4 gap-9 mt-10'> {renderCards}</div>
+        <div className='grid grid-cols-1 md:grid-cols-4 gap-9 mt-20'> {renderCards}</div>
     </div>
     <div className='mt-15 mx-auto w-fit'><Button content="View All" /></div>
     </section>

807ba9b757dbf4d91870e0e40627032c6b28d4c3
Prithviisss <prithvis6002@gmail.com>
2026-01-21 21:21:46 +0530

    complition of process section

--- src/components/Process/Process.jsx
+++ src/components/Process/Process.jsx
@@ -7,13 +7,13 @@ import { CiDeliveryTruck } from "react-icons/ci";
 const Process = () => {
     const renderSteps=steps.map(item=>{
         return(
-            <div>
-                <span>{item.number}</span>
-                <div>
-                    <div><span>{item.icons}</span></div>
-                    <div>
-                        <h4> {item.title}</h4>
-                        <p> {item.para}</p>
+            <div className={`flex-1 ${item.id %2===0? '-mt-50':" "}`}>
+                <span className=' flex justify-center items-center w-18 h-18 rounded-full text-8xl bg-zinc-800  outline-[4px] outline-offset-7 text-white outline-zinc-800 outline-dotted' >{item.number}</span>
+                <div className='flex items-center gap-x-5 mt-10'>
+                    <span className='flex justify-center text-3xl items-center bg-gradient-to-b from-orange-400 to-orange-500 text-white w-15 h-15 rounded-full '>{item.icons}</span>
+                    <div className='flex-1'>
+                        <h4 className='text-zinc-800 text-2xl font-bold'> {item.title}</h4>
+                        <p className='text-zinc-600 mt-2'> {item.para}</p>
                     </div>
                 </div>
             </div>
@@ -24,7 +24,7 @@ const Process = () => {
         <section>
             <div className='max-w-[1400px] mx-auto px-10 py-20' >
                 <div className='w-fit mr-auto '> <Heading highlight="Our  " heading="Process" /></div>
-                <div>
+                <div className='flex items-center justify-center  mt-20 pt-50 '>
                     {renderSteps}
                 </div>
             </div>
